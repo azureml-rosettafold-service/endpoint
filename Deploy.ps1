2930be1802e5569ccb2ffa8f10886935f2b2901d
Dan Miller <danmill@microsoft.com>
2021-08-25 08:06:38 -0700

    Naively copy over from Azure DevOps

---
+++ Deploy.ps1
@@ -0,0 +1,8 @@
+param(
+    $Subscription = "48bbc269-ce89-4f6f-9a12-c6f91fcb772d",
+    $ResourceGroup = "rosettafold2-rg",
+    $Location = "eastus2"
+)
+az account set --subscription $Subscription
+az group create -l $Location -n $ResourceGroup
+az deployment group create -g $ResourceGroup -f azuredeploy.json -p "@azuredeploy.parameters.json"
